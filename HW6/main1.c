de3fc2a7838e391cf817fae98617f5ec646e1dfd
王英瑋 <zihuanfeng520@gmail.com>
2022-12-28 11:33:47 +0800

    Update main1.c

--- HW6/main1.c
+++ HW6/main1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+   int i = 14;
+   if(i % 3==0)
+    printf("Love ");
+   if(i % 5==0)
+    printf("IU");
+    if(i % 3!=0 & i % 5!=0)
+    printf("%d\n", i); 
+ return 0;
+}
