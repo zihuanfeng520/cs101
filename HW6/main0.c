d372c35dcea9842fcd2698f2ca5f9b706229d6d7
王英瑋 <zihuanfeng520@gmail.com>
2022-12-28 11:44:43 +0800

    Update main0.c

--- HW6/main0.c
+++ HW6/main0.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int i=10;
+    
+    if ((i&i-1)==0) {
+        printf("%d is True" ,i);
+    } else {
+        printf("%d is false",i);
+    }
+    return 0;
+}
