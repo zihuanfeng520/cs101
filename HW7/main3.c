47fa389fa5f05d2c0e2b3492b1de3f1cb1411648
王英瑋 <zihuanfeng520@gmail.com>
2022-12-29 14:55:15 +0800

    Create main3.c

---
+++ HW7/main3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    for (int i=1,j=0;i<=9;){
+        printf("%d*%d=%d\t",i,j,i*++j);
+        if(j==9){
+            i++;
+            j=0;
+            printf("\n");
+        }
+    }
+    return 0;
+}
